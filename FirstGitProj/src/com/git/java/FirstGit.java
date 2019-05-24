bac6962ce1958ba80c2e0bd56303c6fecd71f92a
kishupuja <kishupuja89@gmail.com>
2019-05-24 11:27:56 -0700

    I have Added some piece of code

---
+++ FirstGitProj/src/com/git/java/FirstGit.java
@@ -0,0 +1,12 @@
+package com.git.java;
+
+public class FirstGit {
+	public static void main(String[] args) {
+		
+		String s1="kishore has Java Developer";
+		String s2=new String("After 1 week Might be");
+		System.out.println(s1.concat(s2));
+		
+	}
+
+}
